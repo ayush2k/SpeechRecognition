f9afd1f94380cf0e347d37c2d539cadd8f035bc2
Ayush Gupta <ayush2k@gmail.com>
2016-04-21 01:57:18 -0700

    Added Skype Calling

--- SpeechRecog/Form1.Designer.cs
+++ SpeechRecog/Form1.Designer.cs
@@ -31,11 +31,14 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.contactsView = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 226);
+            this.button1.Location = new System.Drawing.Point(333, 226);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -46,7 +49,7 @@
             // button2
             // 
             this.button2.Enabled = false;
-            this.button2.Location = new System.Drawing.Point(197, 226);
+            this.button2.Location = new System.Drawing.Point(445, 226);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
@@ -56,17 +59,50 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Location = new System.Drawing.Point(274, 12);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(260, 208);
             this.richTextBox1.TabIndex = 2;
             this.richTextBox1.Text = "";
             // 
+            // contactsView
+            // 
+            this.contactsView.Location = new System.Drawing.Point(12, 26);
+            this.contactsView.Name = "contactsView";
+            this.contactsView.Size = new System.Drawing.Size(194, 373);
+            this.contactsView.TabIndex = 3;
+            this.contactsView.UseCompatibleStateImageBehavior = false;
+            this.contactsView.View = System.Windows.Forms.View.List;
+            this.contactsView.SelectedIndexChanged += new System.EventHandler(this.contactsView_SelectedIndexChanged);
+            this.contactsView.DoubleClick += new System.EventHandler(this.contactsView_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Contacts";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(332, 304);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(575, 411);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.contactsView);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -75,6 +111,7 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -83,6 +120,9 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ListView contactsView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- SpeechRecog/Form1.cs
+++ SpeechRecog/Form1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 using System.Speech.Recognition;
 using System.Speech.Synthesis;
 using SKYPE4COMLib;
-
+using System.Collections;
 
 namespace SpeechRecog
 {
@@ -18,9 +18,14 @@ namespace SpeechRecog
     {
         SpeechRecognitionEngine engine = new SpeechRecognitionEngine();
         SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+        Skype skype = new Skype();
+        Call call = null;
+        Choices commands = new Choices();
+        List<User> contacts = new List<User>();
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -32,39 +37,67 @@ namespace SpeechRecog
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            Choices commands = new Choices();
-            commands.Add(new String[] { "say hello", "say my name" });
+            commands.Add(new String[] { "say hello", "say my name", "call someone" });
             GrammarBuilder builder = new GrammarBuilder(commands);
             Grammar grammar = new Grammar(builder);
             engine.LoadGrammarAsync(grammar);
             engine.SetInputToDefaultAudioDevice();
-            engine.SpeechRecognized += engine_SpeechRecognized;    
-
-
+            engine.SpeechRecognized += engine_SpeechRecognized;
+            loadContacts();
         }
 
         void engine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
         {
-            switch(e.Result.Text){
-                case("say hello"):
+            switch (e.Result.Text)
+            {
+                case ("say hello"):
                     richTextBox1.Text += "\nsay hello processed";
                     synthesizer.SpeakAsync("Hello! How you doing");
                     break;
 
-                case("say my name"):
+                case ("say my name"):
                     synthesizer.SpeakAsync("\n Hi Ayush");
                     richTextBox1.Text += "\nsay my name processed";
+                    break;
+                case ("call someone"):
+                    string name = "echo123";
+                    User caller = contacts.Where(user => user.FullName.ToLower().IndexOf(name.ToLower()) > -1).
+                                                Select(user => new User
+                                                {
+                                                    Handle = user.Handle
+                                                }).FirstOrDefault();
+                    if (caller != null)
+                    {
+                        skype.PlaceCall(caller.Handle);
+                        synthesizer.SpeakAsync("\n Calling " + name);
+                        richTextBox1.Text += "\nCall processed";
+                    }
+                    else
+                    {
+                        synthesizer.SpeakAsync("Contact Not Found");
+                    }
+                    break;
+                case ("end call"):
+                    if (call != null)
+                    {
+                        call.Finish();
+                        call = null;
+                    }
                     break;
             }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        public void loadContacts()
         {
-            engine.RecognizeAsyncStop();
-            button2.Enabled = false;
-            button1.Enabled = true;
+            contacts.Clear();
+            foreach (User user in skype.Friends)
+            {
+                contacts.Add(user);
+                contactsView.Items.Add(user.FullName);
+            }
         }
-
-
     }
 }
+
+        
+        
