52e3b3a42bf3b0e487ccb71923052ad6744acd0a
bks2009 <bharat.ku.singh@gmail.com>
2016-04-21 20:32:03 -0700

    New APIs with Skype

--- SpeechRecog/Form1.Designer.cs
+++ SpeechRecog/Form1.Designer.cs
@@ -31,16 +31,18 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.contactsView = new System.Windows.Forms.ListView();
             this.label1 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(333, 226);
+            this.button1.Location = new System.Drawing.Point(16, 278);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "Enable Voice Recognition";
             this.button1.UseVisualStyleBackColor = true;
@@ -49,9 +51,10 @@
             // button2
             // 
             this.button2.Enabled = false;
-            this.button2.Location = new System.Drawing.Point(445, 226);
+            this.button2.Location = new System.Drawing.Point(263, 278);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(100, 28);
             this.button2.TabIndex = 1;
             this.button2.Text = "Disable Voice Recognition";
             this.button2.UseVisualStyleBackColor = true;
@@ -59,56 +62,65 @@
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(274, 12);
+            this.richTextBox1.Location = new System.Drawing.Point(16, 15);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(260, 208);
+            this.richTextBox1.Size = new System.Drawing.Size(345, 255);
             this.richTextBox1.TabIndex = 2;
             this.richTextBox1.Text = "";
-            // 
-            // contactsView
-            // 
-            this.contactsView.Location = new System.Drawing.Point(12, 26);
-            this.contactsView.Name = "contactsView";
-            this.contactsView.Size = new System.Drawing.Size(194, 373);
-            this.contactsView.TabIndex = 3;
-            this.contactsView.UseCompatibleStateImageBehavior = false;
-            this.contactsView.View = System.Windows.Forms.View.List;
-            this.contactsView.SelectedIndexChanged += new System.EventHandler(this.contactsView_SelectedIndexChanged);
-            this.contactsView.DoubleClick += new System.EventHandler(this.contactsView_DoubleClick);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 7);
+            this.label1.Location = new System.Drawing.Point(460, 18);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 13);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Contacts";
+            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Car Status";
             // 
-            // button3
+            // label2
             // 
-            this.button3.Location = new System.Drawing.Point(332, 304);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(436, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Bluetooth ---> Off";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(450, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Music ---> Off";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(429, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Navigation ---> Off";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(575, 411);
-            this.Controls.Add(this.button3);
+            this.ClientSize = new System.Drawing.Size(581, 326);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.contactsView);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "x`";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -120,9 +132,10 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.ListView contactsView;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- SpeechRecog/Form1.cs
+++ SpeechRecog/Form1.cs
@@ -10,22 +10,29 @@ using System.Windows.Forms;
 using System.Speech.Recognition;
 using System.Speech.Synthesis;
 using SKYPE4COMLib;
+using System.Diagnostics;
+using System.Threading;
 using System.Collections;
+
 
 namespace SpeechRecog
 {
+
+
     public partial class Form1 : Form
     {
+        Process myMusic;
+        Process myMap;
         SpeechRecognitionEngine engine = new SpeechRecognitionEngine();
         SpeechSynthesizer synthesizer = new SpeechSynthesizer();
         Skype skype = new Skype();
         Call call = null;
         Choices commands = new Choices();
         List<User> contacts = new List<User>();
+
         public Form1()
         {
             InitializeComponent();
-
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -37,25 +44,56 @@ namespace SpeechRecog
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            commands.Add(new String[] { "say hello", "say my name", "call someone" });
+            commands.Add(new String[] { "say hello", "say my name", "navigate to", "tempe", "turn navigation off",
+                "turn blue tooth on", "blue tooth off", "play some music", "stop music",
+                "turn navigation on", "call someone", "end call"});
             GrammarBuilder builder = new GrammarBuilder(commands);
             Grammar grammar = new Grammar(builder);
             engine.LoadGrammarAsync(grammar);
             engine.SetInputToDefaultAudioDevice();
-            engine.SpeechRecognized += engine_SpeechRecognized;
-            loadContacts();
+            engine.SpeechRecognized += engine_SpeechRecognized;    
+
+
         }
 
         void engine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
         {
-            switch (e.Result.Text)
-            {
+            richTextBox1.Text += e.Result.Text;
+            switch (e.Result.Text){
+                case ("turn blue tooth on"):
+                    label2.Text = "Bluetooth ---> On";
+                    break;
+                case ("blue tooth off"):
+                    label2.Text = "Bluetooth ---> Off";
+                    break;
+                case ("play some music"):
+                    label3.Text = "Music ---> On";
+                    myMusic = Process.Start("wmplayer.exe", "C:/Users/Bharat/Downloads/Music/firestone.mp3");
+                    break;
+                case ("stop music"):
+                    label3.Text = "Music ---> Off";
+                    if (myMusic != null)
+                    {
+                        myMusic.CloseMainWindow();
+                        myMusic.Close();
+                    }
+                    break;
+                case ("turn navigation off"):
+                    label3.Text = "Music ---> Off";
+                    if (myMap != null){
+                        myMap.CloseMainWindow();
+                        myMap.Close();
+                    }
+                    break;
+                case ("navigate to"):
+                    richTextBox1.Text += "\nNavigation On";
+                    myMap = Process.Start("https://www.google.com/maps/dir/Tempe/San+Diego");
+                    break;
                 case ("say hello"):
                     richTextBox1.Text += "\nsay hello processed";
                     synthesizer.SpeakAsync("Hello! How you doing");
                     break;
-
-                case ("say my name"):
+                case("say my name"):
                     synthesizer.SpeakAsync("\n Hi Ayush");
                     richTextBox1.Text += "\nsay my name processed";
                     break;
@@ -84,7 +122,17 @@ namespace SpeechRecog
                         call = null;
                     }
                     break;
+                default:
+                    richTextBox1.Text += "\nDefault in";
+                    break;
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            engine.RecognizeAsyncStop();
+            button2.Enabled = false;
+            button1.Enabled = true;
         }
 
         public void loadContacts()
@@ -93,11 +141,8 @@ namespace SpeechRecog
             foreach (User user in skype.Friends)
             {
                 contacts.Add(user);
-                contactsView.Items.Add(user.FullName);
+                //contactsView.Items.Add(user.FullName);
             }
         }
     }
 }
-
-        
-        
